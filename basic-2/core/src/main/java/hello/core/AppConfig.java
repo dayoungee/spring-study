8f30e15d4bce2313466728f95565a4a8d817d683
YunDayoung <dbsek21@naver.com>
2022-04-28 16:02:24 +0900

    롬복에 대해서

--- basic-2/core/src/main/java/hello/core/AppConfig.java
+++ basic-2/core/src/main/java/hello/core/AppConfig.java
@@ -23,7 +23,9 @@ public class AppConfig {
     }
     @Bean
     public OrderService orderService(){
+
         return new OrderServiceImpl(memberRepository(), discountPolicy());
+        //return null;
     }
     @Bean
     public DiscountPolicy discountPolicy(){
