328bcd2ff5cc9ad2b33735cf9ebf70a10236bc6f
YunDayoung <dbsek21@naver.com>
2022-05-11 10:55:34 +0900

    컬렉션 조회 최적화

--- jpa-1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpa-1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,41 +5,62 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 /**
- * V1. 엔티티 직접 노출
+ * V1. 엔티티 직접 노출 / 엔티티를 조회해서 그대로 반환/ 사용하지말자
  * - 엔티티가 변하면 API 스펙이 변한다.
  * - 트랜잭션 안에서 지연 로딩 필요
  * - 양방향 연관관계 문제
  *
- * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+ * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X) 성능이 좋지 않음
  * - 트랜잭션 안에서 지연 로딩 필요
- * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+ *
+ * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O) / 페이징을 못함
  * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경
  가능)
+
+ * V3.1 컬렉션 페이징과 한계 돌파
+ * - 투원 관계는 페치조인으로 쿼리 수 최적화
+ * - 컬렉션은 페치조인 대신에 지연 로딩을 유지하고, default_batch_fetch_size, @BatchSize로 최적화
  *
  * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
  * - 페이징 가능
+ *
  * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
  * - 페이징 가능
+ * - 일대다 관계인 컬렉션은 IN절을 활용해서 메모리에 미리 조회해서 최적화
+ *
  * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
  * - 페이징 불가능...
+ * - join 결과를 그대로 조회 후 앱에서 원한느 모양으로 직접 변환
  *
+ * - 권장 순서-
+ * 1. 엔티티 조회방식으로 우선 접근
+ *   1.페치조인으로 쿼리 수를 최적화
+ *   2.컬렉션 최적화
+ *     1. 페이징 필요 V3.1 사용
+ *     2. 페이징 필요 X 페치조인 사용
+ * 2. 엔티티 조회 방식으로 해결이 안되면 DTO조회 방식 사용
+ * 3. DTO 조회방식으로 해결이 안되면 NativeSQL or 스프링 JdbcTemplate
  */
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
     /**
      * V1. 엔티티 직접 노출
      * - Hibernate5Module 모듈 등록, LAZY=null 처리
@@ -75,6 +96,38 @@ public class OrderApiController {
                 .collect(toList());
         return result;
     }
+
+    @GetMapping("/api/v3.1/orders") // 페치 조인 방법, !V3의 페이징 문제 해결! default_batch_fetch_size: 100 설정을 잡아줘서 N+1문제를 1+1+1 으로 해결, 쿼리 호출 수가 약간 증가하지만, 디비 데이터 전송량이 감소
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    @GetMapping("/api/v4/orders") //N+1문제는 여전히 존재.. 쿼리 루트1번, 컬렉션 N번 실행, 투원관계는 조인해도 데이터 수가 늘어나지 않는 반면, 투매니 관계는 조인하면 증가한다. 그러므로 투원관계는 한번에 조회하고 투매니관계는 메소드를 정의해 그곳에서 처리하도록 한다.
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders") // 쿼리가 한번 나감. v5보다 느릴 수도 있다. 추가 작업도 크고, 페이징이 불가능하다.
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue())).collect(toList());
+    }
+
 
     @Data
     static class OrderDto {
